1ff6c56d9d2676739bbffae131c02e6cb0c96a22
cesarbravoalamo <cesarbravoalamo@gmail.com>
2023-12-19 12:25:28 +0100

    otro commit

---
+++ .vscode/Tipos/01.tipos_avanzados.py
@@ -0,0 +1,22 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "HUAWEI WATCH FIT Special"]
+booleans = [True, False, True, False]
+print(numeros)
+
+print(letras)
+print(booleans)
+matriz = [[0, 1], [2, 0], [3, 0]]
+print(matriz)
+
+
+ceros = [0]*10
+print(ceros)
+
+alfanumerico = numeros + letras
+print(alfanumerico)
+
+
+rango = list(range(10))
+
+print(list(range(1, 11)))
+rango = list()
